10400edaad8072ba5172e490f557598289b85dfd
MarcusVini196 <marcusviniciussolleh@gmail.com>
2024-08-03 14:09:53 -0300

    Add files via upload

--- assets/php/main.php
+++ assets/php/main.php
@@ -1,21 +1,19 @@
 <?php
-
     $nome = addslashes($_POST['nome']);
-    $email = addslashes($_POST['email']);
-    $celular = addslashes($_POST['celular']);
+    $email = addslashes(($_POST['email']));
+    $telefone = addslashes(($_POST['telefone']));
 
     $para = "agenciamv.orcamentos@gmail.com";
-    $assunto = "Orçamentos de Sites ou Loja Virtual - Porfólio";
+    $assunto = "Orçamentos de Sites ou Loja Virtual - Portfólio";
 
-    $corpo = "Nome: ".$nome."\n"."Email:"."\n"."Celular:".$celular;
+   $corpo = "Nome: ".$nome."\n"."Email:"."\n"."Telefone:".$telefone;
 
-    $cabeca = "From: agenciamv.orcamentos@gmail.com"."\n"."Reply-to:".$email."\n"."X=Mailer:PHP/".phpversion();
+   $cabeca = "From: agenciamv.orcamentos@gmail.com"."\n"."Reply-to:".$email."\n"."X=Mailer:PHP/".phpversion();
 
-    if(mail($para, $assunto, $corpo, $cabeca)){
-        echo("E-mail enviado com sucesso");
-    }else {
-        echo("Houve um erro ao enviar o email!");
-    }
-
+   if(mail($para, $assunto, $corpo, $cabeca)){
+    echo("Email enviado com sucesso");
+   } else {
+        echo("Houve umm erro ao enviar o email");
+   }
 
 ?>
