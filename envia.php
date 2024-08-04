f1fb228f6d2d46408a41c7f38ce5377d918f026b
MarcusVini196 <marcusviniciussolleh@gmail.com>
2024-08-04 19:56:42 -0300

    Add files via upload

--- envia.php
+++ envia.php
@@ -1,35 +1,21 @@
 <?php
-  //Variáveis
-  $nome = $_POST['nome'];
-  $email = $_POST['email'];
-  $celular =$_POST['celular'];
-  $mensagem = $_POST['mensagem'];
-  $data_envio = date('d/m/Y');
-  $hora_envio = date('H:i:s');
 
-  //Compo E-mail
-  $arquivo = "
-    <html>
-      <p><b>Nome: </b>$nome</p>
-      <p><b>E-mail: </b>$email</p>
-      <p><b>Celular: </b>$celular</p>
-      <p><b>Mensagem: </b>$mensagem</p>
-      <p>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></p>
-    </html>
-  ";
-  
-  //Emails para quem será enviado o formulário
-  $destino = "agenciamv.orcamentos@gmail.com";
-  $assunto = "Contato pelo Portfólio";
+    $nome = addslashes($_POST['nome']);
+    $email = addslashes($_POST['email']);
+    $celular = addslashes($_POST['celular']);
+    $mensagem = addslashes($_POST['mensagem']);
 
-  //Este sempre deverá existir para garantir a exibição correta dos caracteres
-  $headers  = "MIME-Version: 1.0\n";
-  $headers .= "Content-type: text/html; charset=iso-8859-1\n";
-  $headers .= "From: $nome <$email>";
+    $para = "agenciamv.orcamentos@gmail.com";
+    $assunto = "Orçamento de sites - Portfólio";
 
-  //Enviar
-  mail($destino, $assunto, $arquivo, $headers);
-  
-  echo "<meta http-equiv='refresh' content='10;URL=../contato.html'>";
+    $corpo = "Nome: ".$nome."\n"."Email: ".$email."\n"."Celular: ".$celular."\n"."Mensagem: ".$mensagem;
+    $cabeca = "From: agenciamv.orcamentos@gmail.com"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion();
+
+    if(mail($para, $assunto, $corpo, $cabeca)){
+        echo("Email enviado com sucesso!");
+    }else {
+        echo("Houve um erro ao enviar o email!");
+    }
+
 
 ?>
